cc7db80ad3f8f8f16faa9f1d171e4717683b2d28
Amar <amar.bapu@gmail.com>
2018-08-22 14:13:18 +0530

    Adding config parsing masked out prop stuff

--- cmd/tpz-geocalc/main.go
+++ cmd/tpz-geocalc/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
+	"os"
+	"path"
 	"time"
 
+	"github.com/BurntSushi/toml"
 	"github.com/bamarb/aws-pipeline-go/pkg/task"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/mediocregopher/radix.v3"
+	log "github.com/sirupsen/logrus"
 )
 
 /* Config Types */
@@ -13,7 +20,7 @@ import (
 // Database struct to hold db conn info
 type Database struct {
 	Server   string
-	Port     int
+	Port     string
 	Dbname   string
 	User     string
 	Password string
@@ -29,10 +36,10 @@ type OutputInfo struct {
 
 // Config config struct decoded from toml
 type Config struct {
-	Version   string
-	Radius    string
-	Output    OutputInfo
-	Databases map[string]Database
+	Version string
+	Radius  string
+	Output  OutputInfo
+	Db      map[string]Database
 }
 
 var (
@@ -40,14 +47,47 @@ var (
 	startTime  = time.Now()
 	workerPool *task.Pool
 	redisPool  *radix.Pool
-	conf       Config
+	config     *Config
 )
 
 func init() {
 	flag.StringVar(&cfgFile, "f", "config.toml", "# Path to cfg file (.toml)")
 }
 
+func configLogging(logCfg OutputInfo) {
+	if err := os.MkdirAll(logCfg.Logdir, 0755); err != nil {
+		fmt.Printf("Error creating log directory: %s", err)
+		os.Exit(2)
+	}
+	logfile := path.Join(logCfg.Logdir, logCfg.Logfile)
+	f, err := os.OpenFile(logfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
+	if err != nil {
+		fmt.Printf("Error creating log file: %s", err)
+		os.Exit(2)
+	}
+	log.SetFormatter(&log.TextFormatter{})
+	log.SetOutput(f)
+	log.SetLevel(log.DebugLevel)
+}
+
 func main() {
 	flag.Parse()
+	if _, err := toml.DecodeFile(cfgFile, &config); err != nil {
+		fmt.Printf("FATAL error parsing cfg file : %s ", err)
+		os.Exit(1)
+	}
+	configLogging(config.Output)
+
+	// Todo Pull this from config or ENV
+	dbCfg := config.Db["mysql-prod"]
+	sqlConnStr := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbCfg.User,
+		dbCfg.Password, dbCfg.Server, dbCfg.Port, dbCfg.Dbname)
+
+	db, err := sql.Open("mysql", sqlConnStr)
+	if err != nil {
+		log.Error(err)
+		os.Exit(3)
+	}
+	defer db.Close()
 
 }
